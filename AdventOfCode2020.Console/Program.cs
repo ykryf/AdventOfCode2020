08d97f64f3795cd8364d339004b4e8b5d52c96d1
Yiannis Kryfos <yianniskryfos@yahoo.com>
2020-12-12 16:47:28 +0200

    Add project files.

---
+++ AdventOfCode2020.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2020.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
